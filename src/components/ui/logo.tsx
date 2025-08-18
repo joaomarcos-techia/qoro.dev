3a1214bc31c2480534cca05cc931643677c1a05b
João Marcos <joaomarcosrodduarte@gmail.com>
2025-08-18 02:19:30 +0000

    erro de mostrar a logo persiste, O 404 significa que a imagem não foi en

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import Image from 'next/image';
@@ -10,7 +11,7 @@ interface LogoProps {
 export function Logo({ width = 120, height = 32 }: LogoProps) {
   return (
     <Image
-      src="https://firebasestorage.googleapis.com/v0/b/qoro-iy1gs.appspot.com/o/qoro-logo.png?alt=media&token=425a8155-c543-4340-a1f7-e722c83675a8"
+      src="/logo.svg"
       alt="Qoro Logo"
       width={width}
       height={height}
