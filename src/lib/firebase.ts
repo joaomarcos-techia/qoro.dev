ba5717f56b5af719200f5e8544812c4de1cf3a7d
João Marcos <joaomarcosrodduarte@gmail.com>
2025-09-09 23:47:21 +0000

    e sobre a segurança. algo esta visivel no frontend? como id do usuario,

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,14 +7,15 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "qoro-iy1gs",
-  "appId": "1:62039220673:web:c357a855b1093c508f8f40",
-  "storageBucket": "qoro-iy1gs.firebasestorage.app",
-  "apiKey": "AIzaSyB-v1ynnLnRuIV4-VTJhBtXPPkHpg-ivtU",
-  "authDomain": "qoro-iy1gs.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "62039220673"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
